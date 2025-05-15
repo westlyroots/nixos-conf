19bdb9b540bf01422f65b7b40057c7123a6c04a7
westlyroots <westlyroots@proton.me>
2025-05-15 02:49:56 -0400

    telegram

--- modules/home.hyprland.nix
+++ modules/home.hyprland.nix
@@ -6,12 +6,19 @@
   # set the Hyprland and XDPH packages to null to use the ones from the NixOS module
   package = null;
   portalPackage = null;
+  xwayland.enable = true;
   settings = {
    "$mod" = "SUPER";
+   "$terminal" = "kitty";
+    exec-once = [
+    "waybar"
+    ];
     bind =
       [
         "$mod, F, exec, firefox"
-      ]
+        "$mod, T, exec, $terminal"
+        "$mod, M, exit"
+        ]
       ++ (
         # workspaces
         # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
@@ -24,7 +31,21 @@
           )
           9)
       );
+       bindm = [
+    # mouse movements
+    "$mod, mouse:272, movewindow"
+    "$mod, mouse:273, resizewindow"
+    "$mod ALT, mouse:272, resizewindow"
+    ];
+    monitor = [
+    ", preferred, auto, 1"];
+    general = {
+    allow_tearing = true;
+    };
       };
+};
+  programs.waybar = {
+  enable = true;
   };
   # Optional, hint Electron apps to use Wayland:
   # home.sessionVariables.NIXOS_OZONE_WL = "1";
