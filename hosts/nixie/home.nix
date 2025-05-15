575efc80b3a0e7259eb3d46155ea66a34370eb00
westlyroots <westlyroots@proton.me>
2025-05-15 02:55:29 -0400

    addgit credentials manager

--- hosts/nixie/home.nix
+++ hosts/nixie/home.nix
@@ -38,6 +38,7 @@ nixpkgs.config = {
      zsh-powerlevel10k
      lutris
      telegram-desktop
+     git-credential-keepassxc
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
