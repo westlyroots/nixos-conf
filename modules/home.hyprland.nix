d3072e24722bd46bf3d2dcbd260112e1c75073ff
westlyroots <westlyroots@proton.me>
2025-05-16 03:41:55 -0400

    more hyprland config yayyy

--- modules/home.hyprland.nix
+++ modules/home.hyprland.nix
@@ -21,6 +21,7 @@
         "size 622 652, class:(clipse)"
         "content game, class:^steam_app\d+$"
         "workspace 3, class:keepassxc"
+	"content game, class:(ffxiv_dx11.exe)"
       ];
       bind =
         [
@@ -46,14 +47,23 @@
             )
             9)
         );
-      bindm = [
+	bindel = [
+	  ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+	  ", XF86AudioLowerVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%-"
+	  ];
+	bindl = [
+	", XF86AudioPlay, exec, playerctl play-pause"
+	", XF86AudioPrev, exec, playerctl previous"
+	", XF86AudioNext, exec, playerctl next"
+	];
+	bindm = [
         # mouse movements
         "$mod, mouse:272, movewindow"
         "$mod, mouse:273, resizewindow"
         "$mod ALT, mouse:272, resizewindow"
       ];
       monitor = [
-        ", preferred, auto, 1"];
+        ", highrr, auto, 1"];
       general = {
         allow_tearing = true;
         gaps_in = 5;
@@ -74,6 +84,10 @@
         color = "rgba(1a1a1aee)";
         };
         };
+	input = {
+	  sensitivity = 0.0;
+	  accel_profile = "flat";
+	};
       dwindle = {
         pseudotile = true;
         preserve_split = true;
