868951d7f71d33433dcca3ed429d61286b76e6f9
westlyroots <westlyroots@proton.me>
2025-05-24 03:26:59 -0400

    add cmus

--- hosts/nixie/home.nix
+++ hosts/nixie/home.nix
@@ -40,6 +40,7 @@ nixpkgs.config = {
      telegram-desktop
      git-credential-keepassxc
      protonup-qt
+     cmus
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
