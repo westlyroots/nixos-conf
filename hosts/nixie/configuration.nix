b3552f613cfd1fc42746d6084d7ca9e168129722
westlyroots <westlyroots@proton.me>
2025-07-02 22:35:40 -0400

    add nfs export to local drive

--- hosts/nixie/configuration.nix
+++ hosts/nixie/configuration.nix
@@ -105,7 +105,30 @@
     ];
   };
 
+fileSystems."/export/test" = {
+	device = "/dev/sdb1";
+	#options = [  ];
+	};
 
+services.nfs.server = {
+    enable = true;
+    # fixed rpc.statd port; for firewall
+    lockdPort = 4001;
+    mountdPort = 4002;
+    statdPort = 4000;
+    extraNfsdConfig = '''';
+  };
+  networking.firewall = {
+    enable = true;
+      # for NFSv3; view with `rpcinfo -p`
+    allowedTCPPorts = [ 111  2049 4000 4001 4002 20048 ];
+    allowedUDPPorts = [ 111 2049 4000 4001  4002 20048 ];
+  };
+services.nfs.server.exports = ''
+	/export *(insecure,rw,sync,no_subtree_check,crossmnt,fsid=0)
+	/export/test *(insecure,rw,async,no_subtree_check)
+
+'';
 
   # Install firefox.
 
